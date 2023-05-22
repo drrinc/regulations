37e6f197279e10cd47891d8c50bd87acbf0def64
drrinc <drinck@protonmail.com>
2023-05-23 00:27:20 +0900

    Update cr.py

--- cr.py
+++ cr.py
@@ -3,23 +3,19 @@ from bs4 import BeautifulSoup
 import re
 
 
-html = urlopen("https://www.db.yugioh-card.com/yugiohdb/member_deck.action?ope=1&cgid=378c03716b23a8acc15d78df038b68ac&dno=129&request_locale=ko")  
-bsObject = BeautifulSoup(html, "html.parser")
-htmlparsed = bsObject.select('a[target][href]')
 
-cid = []
-cname = []
-
-for text in htmlparsed:
-    cid.append(''.join(re.findall('cid\=(.+)\&amp', str(text))))
-    cname.append(''.join(re.findall('title="(.+)"\/>', str(text))))
-
-cid = list(filter(None, cid))
-cname = list(filter(None, cname))
-
-
-print(cid)
-print(cname)
+def crdb(url):
+    html = urlopen(url)  
+    bsObject = BeautifulSoup(html, "html.parser")
+    htmlparsed = bsObject.select('a[target][href]')
+    cid = []
+    cname = []
+    for text in htmlparsed:
+        cid.append(''.join(re.findall('cid\=(.+)\&amp', str(text))))
+        cname.append(''.join(re.findall('title="(.+)"\/>', str(text))))
+    cid = list(filter(None, cid))
+    cname = list(filter(None, cname))
+    return cid, cname
 
 
 
